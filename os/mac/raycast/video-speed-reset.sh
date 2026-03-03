39c36a98b779ba1391d8be5b1270dc4433f9b048
Ferran Martín <ferranmatin@gmail.com>
2026-03-03 18:20:56 +0100

    Add raycast script to control video speeds

---
+++ os/mac/raycast/video-speed-reset.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Video Speed Reset
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Video Speed
+
+# Documentation:
+# @raycast.description Reset video playback speed to 1x
+# @raycast.author FerranMartin
+# @raycast.authorURL https://raycast.com/FerranMartin
+
+osascript <<'EOF'
+tell application "System Events"
+	set frontApp to name of first application process whose frontmost is true
+end tell
+
+set jsCode to "var v = document.querySelector('video'); if(v){ v.playbackRate = 1; } else { 'No video element found'; }"
+
+if frontApp contains "Arc" then
+	tell application "Arc"
+		tell active tab of window 1 to execute javascript jsCode
+	end tell
+else if frontApp contains "Chrome" then
+	tell application "Google Chrome"
+		tell active tab of window 1 to execute javascript jsCode
+	end tell
+else if frontApp contains "Brave" then
+	tell application "Brave Browser"
+		tell active tab of window 1 to execute javascript jsCode
+	end tell
+else if frontApp contains "Edge" then
+	tell application "Microsoft Edge"
+		tell active tab of window 1 to execute javascript jsCode
+	end tell
+else if frontApp contains "Safari" then
+	tell application "Safari"
+		set jsResult to do JavaScript jsCode in front document
+	end tell
+else
+	log "Unsupported app: " & frontApp
+end if
+EOF
---
+++ os/mac/raycast/video-speed-up.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Video Speed Up
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Video Speed
+
+# Documentation:
+# @raycast.description Increase video playback speed by 0.25x
+# @raycast.author FerranMartin
+# @raycast.authorURL https://raycast.com/FerranMartin
+
+osascript <<'EOF'
+tell application "System Events"
+	set frontApp to name of first application process whose frontmost is true
+end tell
+log "Front app: " & frontApp
+
+set jsCode to "var v = document.querySelector('video'); if(v){ var prev = v.playbackRate; v.playbackRate = Math.min(v.playbackRate + 0.25, 4); 'Changed from ' + prev + ' to ' + v.playbackRate; } else { 'No video element found'; }"
+
+if frontApp contains "Arc" then
+	tell application "Arc"
+		tell active tab of window 1 to execute javascript jsCode
+	end tell
+else if frontApp contains "Chrome" then
+	tell application "Google Chrome"
+		tell active tab of window 1 to execute javascript jsCode
+	end tell
+else if frontApp contains "Brave" then
+	tell application "Brave Browser"
+		tell active tab of window 1 to execute javascript jsCode
+	end tell
+else if frontApp contains "Edge" then
+	tell application "Microsoft Edge"
+		tell active tab of window 1 to execute javascript jsCode
+	end tell
+else if frontApp contains "Safari" then
+	tell application "Safari"
+		set jsResult to do JavaScript jsCode in front document
+	end tell
+else
+	log "Unsupported app: " & frontApp
+end if
+EOF
